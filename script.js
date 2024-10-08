4b1964a8562bb3d83552b9d43302de1f41c5fdc9
oracle <nwazotachibuike@gmail.com>
2024-10-08 23:32:14 +0100

    added script js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // The DOMContentLoaded event fires when the HTML document has been completely parsed
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", function (event) {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
